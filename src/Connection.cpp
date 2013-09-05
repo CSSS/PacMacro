6d59e3b1fe59c4f7b8073eb667d3d703738e6fce
Joel Teichroeb <joel@teichroeb.net>
2013-09-05 14:57:34 -0700

    Increase buffer size

--- src/Connection.cpp
+++ src/Connection.cpp
@@ -12,7 +12,7 @@ Connection::Connection() {
 }
 
 void Connection::send(const std::string &data) {
-	unsigned char buf[LWS_SEND_BUFFER_PRE_PADDING + 512 + LWS_SEND_BUFFER_POST_PADDING];
+	unsigned char buf[LWS_SEND_BUFFER_PRE_PADDING + 2048 + LWS_SEND_BUFFER_POST_PADDING];
 	unsigned char *p = &buf[LWS_SEND_BUFFER_PRE_PADDING];
 	size_t size = data.size();
 	memcpy(p, data.c_str(), size);
